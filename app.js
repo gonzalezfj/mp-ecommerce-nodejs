14aa3bf271da9c07be4ce1155c2ec8a6480802ee
Facundo J Gonzalez <gonzalezfj@unsj-cuim.edu.ar>
2020-08-21 00:16:11 -0300

    checkout fix price and quantity 3

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var exphbs  = require('express-handlebars');
 var mercadopago = require ('mercadopago');
  
 var app = express();
+app.use(express.bodyParser());
 
 mercadopago.configure({
     access_token: 'APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398',
